f2fea03aee42d45c24ada54594ea717e98ed6230
Megan <meganjacob2004@yahoo.com>
2025-06-21 23:42:57 -0700

    move keys to .env file

--- src/components/stretch/VoiceAssistant.js
+++ src/components/stretch/VoiceAssistant.js
@@ -11,8 +11,8 @@ const VoiceAssistant = () => {
 
   useEffect(() => {
     console.log('[VoiceAssistant] Starting to load Vapi SDK...');
-    const apiKey = ''; // ADD API KEY
-    const assistant = ''; // ADD ASSITANT KEY
+    const apiKey = process.env.VAPI_API_KEY;
+    const assistant = process.env.VAPI_ASSISTANT_ID;
     const customPrompt = 'I have slouched shoulders and my back is bent.';
 
     loadVapi({
